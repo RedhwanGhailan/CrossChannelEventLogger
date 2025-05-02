94e2783aea9dc92362de44769b44b1cebaed3f82
Redhwan <redhwan.ghailan@gmail.com>
2025-05-02 12:32:50 +0200

    add the logger manager that run mutli log parallel

---
+++ EventLoggerLib/LoggerManager.cs
@@ -0,0 +1,28 @@
+﻿using EventLoggerLib.Logging;
+
+namespace EventLoggerLib;
+
+public class LoggerManager
+{
+    private readonly List<ILogger> _loggers = [];
+
+    public void AddLogger(ILogger logger)
+    {
+        _loggers.Add(logger);
+    }
+
+    public async Task ParallelLogAsync(string message, LogLevel level = LogLevel.Information)
+    {
+        var entry = new LogModel { Message = message, Level = level };
+
+        await Parallel.ForEachAsync(_loggers, async (logger, _) =>
+        {
+            await logger.LogAsync(entry);
+        });
+    }
+}
+
+
+
+
+
